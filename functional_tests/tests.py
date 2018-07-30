1b819194f2b57596a0f998a46c3663f2dfc23028
enen <844316200@qq.com>
2018-07-30 17:30:48 +0800

    7.3修改

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,16 +4,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 from django.test import LiveServerTestCase
 from selenium.common.exceptions import WebDriverException
-# browser = webdriver.Firefox()
-# browser.get('http://localhost:8000')
 
-# assert 'To-Do' in browser.title
 
-# browser.quit()
-
+MAX_WAIT = 10
 class NewVisitorTest(LiveServerTestCase):
-
-	MAX_WAIT = 10
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -22,28 +16,23 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.quit()
 
 	def wait_for_row_in_list_table(self, row_text):
-		# table = self.browser.find_element_by_id('id_list_table')
-		# rows = self.browser.find_elements_by_tag_name('tr')
-		# self.assertIn(row_text, [row.text for row in rows])
-
 		start_time = time.time()
 		while True:
 			try:
 				table = self.browser.find_element_by_id('id_list_table')
-				rows = self.browser.find_elements_by_tag_name('tr')
+				rows = table.find_elements_by_tag_name('tr')
 				self.assertIn(row_text, [row.text for row in rows])
 				return
 			except (AssertionError, WebDriverException) as e:
-				if time.time-start_time>MAX_WAIT:
+				if time.time()-start_time>MAX_WAIT:
 					raise e
 				time.sleep(1)
 		
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
+	def aaatest_can_start_a_list_and_retrieve_it_later(self):
 		#有一个在线办事应用
 		#去看了应用首页
 		self.browser.get(self.live_server_url)
-		# self.browser.get('http://localhost:8000')
 
 		#注意到网站标题包含“To-Do”
 		self.assertIn('To-Do', self.browser.title)
@@ -61,7 +50,6 @@ class NewVisitorTest(LiveServerTestCase):
 		#她按回车后页面更新
 		#待办事项表格中显示了"1:Buy peacock feathers"
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(2) 
 		self.wait_for_row_in_list_table('1:Buy peacock feather')
 
 
@@ -71,32 +59,84 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(2)
-		
-		#页面再次刷新，清单中显示了这来两个事项
-		# table = self.browser.find_element_by_id('id_list_table')
-		# rows = table.find_elements_by_tag_name('tr')
-		#self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows),"New to-do item did not appear in table")
-		# self.assertTrue(
-		# 	any(row.text == '1:Buy peacock feathers' for row in rows), 
-		# 	f"New to-do item did not in table.\n Content were:\n{table.text}"
-		# 	)
 
-		# self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-
-
-		# table = self.browser.find_element_by_id('id_list_table')
-		# rows = table.find_elements_by_tag_name('tr')
-		# self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-		# self.assertIn('2.Use peacock feathers to make a fl', [row.text for row in rows])
-
-
-		self.wait_for_row_in_list_table('1:Buy peacock feather')
 		self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1:Buy peacock feather')
 
 		self.fail('Finish the Test!')
 
 
 
-# if __name__ == '__main__':
-# 	unittest.main(warnings='ignore')
+	def test_can_start_a_list_one_user(self):
+		#有一个在线办事应用
+		#去看了应用首页
+		self.browser.get(self.live_server_url)
+		
+		#应用邀请他输入一个待办事项
+		#她在一个文本框中输入“Buy peacock feathers”
+		#她的爱好是使用假蝇做鱼饵钓鱼
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feather')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1:Buy peacock feather')
+		#页面中又显示了一个文本框，可以输入其他的待办事项
+		#她输入了“Use peacock feathers to make a fly”
+		#伊迪斯做事很有条理
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		#页面再次刷新，清单中显示了这来两个事项
+		self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1:Buy peacock feather')
+		
+
+
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		#伊迪斯新建一个待办事项 
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+		#她注意到清单有个唯一的url
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+
+
+		#现在一名叫做弗朗西斯的新用户访问了网站
+
+		##使用一个新的会话
+		##确保弗朗西斯的信息不回从cookie中泄露出去
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		#弗朗西斯访问首页
+		#页面中看不到伊迪斯的清单
+		self.browser.get(live_server_url)
+		page_text = self.browser.get_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		assertNotIn('make a fly', page_text)
+
+		#弗朗西斯输入一个新待办事项，新建一个清单
+		#他没有伊迪斯那样兴趣盎然
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		#弗朗西斯获得了唯一URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertEqual(francis_list_url, edith_list_url)
+
+		#这个页面还是没有伊迪斯的清单内容
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertIn('Buy peacock feather', page_text)
+		self.assertIn('Buy milk', page_text)
+
+
+		#两人都很满意，然后休息去了
+
--- lists/tests.py
+++ lists/tests.py
@@ -5,13 +5,6 @@ from django.http import HttpRequest
 from lists.models import Item
 
 # Create your tests here.
-
-# class SmokeTest(TestCase):
-# 	"""docstring for SmokeTest"""
-	
-# 	def test_bad_maths(self):
-# 		self.assertEqual(1+1, 3)
-
 class ItemModelTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
@@ -35,24 +28,6 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 
-	# def test_root_url_resolves_to_home_page_view(self):
-	# 	found = resolve('/')
-	# 	self.assertEqual(found.func, home_page)
-
-
-	# def test_home_page_returns_correct_html(self):
-	# 	# request = HttpRequest()
-	# 	# response = home_page(request)
-	# 	# html = response.content.decode('utf-8')
-	# 	# self.assertTrue(html.startswith('<!DOCTYPE html>'))
-	# 	# self.assertIn('<title>To-Do lists</title>', html)
-	# 	# self.assertTrue(html.endswith('</html>'))
-	# 	response = self.client.get('/')
-	# 	html = response.content.decode('utf8')
-	# 	self.assertTrue(html.startswith('<!DOCTYPE html>'))
-	# 	self.assertIn('<title>To-Do lists</title>', html)
-	# 	self.assertTrue(html.endswith('</html>'))
-
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
@@ -60,21 +35,16 @@ class HomePageTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
-		# self.assertIn('A new list item', response.content.decode('utf8'))
-		# self.assertTemplateUsed(response, 'home.html')
 
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
-		# self.assertIn('A new list item', response.content.decode())
-		# self.assertTemplateUsed(response, 'home.html')
-
 
 	def test_redirect_after_POST(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
 	def test_displays_all_list_items(self):
